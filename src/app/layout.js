975b249823259a612f21fa22dfd9a6893d197e46
{M Saad} <{msd08@gmail.com}>
2025-07-20 15:03:03 +0500

    final changes including moving of favicon.ico

--- src/app/layout.js
+++ src/app/layout.js
@@ -61,7 +61,6 @@ export const metadata = {
     shortcut: '/favicon-16x16.png',
     apple: '/apple-touch-icon.png',
   },
-  manifest: '/site.webmanifest',
 };
 
 export default function RootLayout({ children }) {
